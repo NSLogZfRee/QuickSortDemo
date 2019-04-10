4056ecc46042bd8f7a97f5706e37db742869c1ff
ZfRee <imzfree@163.com>
2019-04-10 16:29:49 +0800

    Initial Commit

---
+++ QuickSortDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  QuickSortDemo
+//
+//  Created by ZfRee on 2019/4/10.
+//  Copyright © 2019年 ZfRee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
